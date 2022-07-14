cd46152b9c0489d02f7fa71f22c3b5382f1f21f8
Bell <jacobbrister71@gmail.com>
2022-07-14 17:21:07 -0400

    added ability to load tasks from local storage and display them on start up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var currentTime = moment();
+var savedTasks = [];
 
 var displayCalendar = function() {
     var currentDate = moment().format("dddd, MMMM Do YYYY");
@@ -23,17 +24,18 @@ var createCalendarElements = function() {
         }
         calendarLeft.addClass("calendar-left hour col-1 mb-1");
 
-        var hourClass = "hour-" + i;
+        var hourId = i;
         var calendarCenter = $("<div></div>");
-        calendarCenter.addClass("calendar-center col-10 mb-1" + hourClass);
+        calendarCenter.addClass("calendar-center col-10 mb-1 hour-" + hourId);
+        calendarCenter.attr("hour", hourId);
         var textContainer = $("<p>");
         textContainer.addClass("text-container");
         calendarCenter.append(textContainer);
         getTimeColor(calendarCenter, i+9);
 
         var calendarRight = $("<div></div>");
-        calendarRight.addClass("calendar-right col-1 saveBtn d-flex align-items-center justify-content-center");
-        var icon = $("<span></span>");
+        calendarRight.addClass("calendar-right col-1 saveBtn mb-1 d-flex align-items-center justify-content-center");
+        var icon = $("<i></i>");
         icon.addClass("oi oi-document");
         calendarRight.append(icon);
 
@@ -88,4 +90,38 @@ $(".container").on("blur", "textarea", function() {
     $(this).replaceWith(textContainer);
 });
 
+$(".container").on("click", ".calendar-right", function() {
+    var calendarCenter = $(this).parent().find(".calendar-center");
+    var textContainer = calendarCenter.find("p");
+
+    console.log(calendarCenter.attr("hour"));
+
+    if(textContainer.text()) {
+        var taskObj = {
+            text: textContainer.text(),
+            hour: calendarCenter.attr("hour")
+        }
+
+        savedTasks.push(taskObj);
+        localStorage.setItem("tasks", JSON.stringify(savedTasks));
+    }
+});
+
+var loadTasks = function() {
+    savedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+    console.log("here");
+
+    for(var i = 0; i < savedTasks.length; i++) {
+        console.log("got here");
+        var calString = ".hour-" + savedTasks[i].hour;
+        console.log(calString);
+        var calendarElement = $(calString);
+        calendarElement.text(savedTasks[i].text);
+        console.log(calendarElement);
+    }
+};
+
 displayCalendar();
+
+loadTasks();
